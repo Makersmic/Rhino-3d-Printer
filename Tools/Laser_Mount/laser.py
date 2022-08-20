9b63440cf60dfd263f73339a4d6c7fae6425f206
Makersmic <58558877+Makersmic@users.noreply.github.com>
2022-08-20 11:48:10 -0500

    Update laser.py

--- Tools/Laser_Mount/laser.py
+++ Tools/Laser_Mount/laser.py
@@ -1,1 +1,8 @@
+# The Laser:
+Current primary functions of the laser attachment are cutting of vector based shapes out of flat sheet material.  Rhino 
+firmware has not been optimised for engraving of raster images.
 
+Laser attachment functions through the available 12v supply.
+
+Rhino has been optimised to work with Laserweb but is not limited by its usage allowing for other
+Laser control software.
